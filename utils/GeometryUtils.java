dc3300d115c578ea6fa9a1768ee602104cfd72cb
nghiatt <nghiatt1030@gmail.com>
2018-08-22 20:01:02 +0900

    Add some utility methods

--- utils/GeometryUtils.java
+++ utils/GeometryUtils.java
@@ -16,4 +16,16 @@ public class GeometryUtils {
     public static final double absoluteAngle(Point2D source, Point2D target) {
         return FastTrig.atan2(target.getX() - source.getX(), target.getY() - source.getY());
     }
+	
+	public static double bulletVelocity(double power) {
+		return 20 - 3 * power;
+	}
+	
+	public static int sign(double v) {
+		return v < 0 ? -1 : 1;
+	}
+	
+	public static int boundedValue(int v, int min, int max) {
+		return Math.max(min, Math.min(max, v));
+	}
 }

fe98b108717a9159111bea4b46473aa72cd123a4
nghiatt <nghiatt1030@gmail.com>
2018-08-22 21:56:48 +0900

    Update more info when scanned a bot

--- Enemy.java
+++ Enemy.java
@@ -6,12 +6,24 @@ import java.awt.geom.*;
 
 public class Enemy {
 	public String name;
-	public double lastBearing;
 	public double bearing;
-	public double lastHeading;
 	public double heading;	// enemy heading
 	public int ctime;      // game time that the scan was produced
 	public double speed;
 	public Point2D.Double location = new Point2D.Double();
 	public double distance;
+    public Enemy lastState;
+    
+    public void updateLastState() {
+        if (lastState == null) {
+            lastState = new Enemy();
+            lastState.name = name;
+        }
+        lastState.bearing = bearing;
+        lastState.heading = heading;
+        lastState.ctime = ctime;
+        lastState.speed = speed;
+        lastState.location = location;
+        lastState.distance = distance;
+    }
 }
--- radar/MeleeRadar.java
+++ radar/MeleeRadar.java
@@ -14,29 +14,29 @@ import unnamed.utils.*;
  * Shamelessly stole from DrussGT. All credits to the original author Skilgannon.
  */
 public class MeleeRadar extends BaseRadar {
-	
+    
     private Point2D.Double _myLocation;
-	
+    
     public MeleeRadar(AdvancedRobot robot) {
         super(robot);
     }
-	
-	/*
-	 * What the radar does every tick, if no other event occured.
-	 */
-	@Override
-	public void onTick() {
+    
+    /*
+     * What the radar does every tick, if no other event occured.
+     */
+    @Override
+    public void onTick() {
         if (_robot.getRadarTurnRemaining() == 0)
-			// If radar stopped for some reason,
-			// turn it around to find new target
+            // If radar stopped for some reason,
+            // turn it around to find new target
             _robot.setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
         
-		// Track my own location
+        // Track my own location
         _myLocation =  new Point2D.Double(_robot.getX(), _robot.getY());   
     }
-	
-	@Override
-	public void onScannedRobot(ScannedRobotEvent e) {   
+    
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {   
         String enemyName = e.getName();
                             
         Enemy enemyInfo = Memory.enemies.get(enemyName);
@@ -45,38 +45,45 @@ public class MeleeRadar extends BaseRadar {
             enemyInfo.name = enemyName;
         }
         
+        enemyInfo.updateLastState();
         enemyInfo.ctime = (int)_robot.getTime();
-        double absoluteBearing  = _robot.getHeadingRadians() + e.getBearingRadians();
-        enemyInfo.location = GeometryUtils.project(_myLocation, absoluteBearing, e.getDistance());
-            
+        enemyInfo.bearing  = _robot.getHeadingRadians() + e.getBearingRadians();
+        // TODO: enemyInfo.heading
+        enemyInfo.speed = e.getVelocity();
+        enemyInfo.distance = e.getDistance();
+        enemyInfo.location = GeometryUtils.project(_myLocation, enemyInfo.bearing, enemyInfo.distance);
+        
         if (_robot.getOthers() <= Memory.enemies.size()) {
             Enumeration<Enemy> all = Memory.enemies.elements();
             int oldestScan = enemyInfo.ctime;
             while (all.hasMoreElements()) {
                 Enemy tmp = all.nextElement();
                 if (tmp.ctime < oldestScan) {
-					absoluteBearing = GeometryUtils.absoluteAngle(_myLocation, tmp.location);
-					oldestScan = tmp.ctime;
+                    enemyInfo.bearing = GeometryUtils.absoluteAngle(_myLocation, tmp.location);
+                    oldestScan = tmp.ctime;
                 }
             }
             if (_robot.getOthers() == 1 && oldestScan == enemyInfo.ctime) {
-				// 1 vs 1: Track the only remaining enemy
-                double angle = Utils.normalRelativeAngle(absoluteBearing - _robot.getRadarHeadingRadians());
-				// TODO: Investigate
-                _robot.setTurnRadarRightRadians(Math.signum(angle)*Math.min(Math.abs(angle) + (Math.PI/4 - Math.PI/8 - Math.PI/18), Math.PI/4));
+                // 1 vs 1: Track the only remaining enemy
+                double radarOffset = Utils.normalRelativeAngle(enemyInfo.bearing - _robot.getRadarHeadingRadians());
+                // TODO: Investigate
+                _robot.setTurnRadarRightRadians(Math.signum(radarOffset)
+                                                * Math.min(Math.abs(radarOffset) + (Math.PI/4 - Math.PI/8 - Math.PI/18),
+                                                           Math.PI/4));
             }
             else
-				// Melee
-				// TODO: Investigate
-                _robot.setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - _robot.getRadarHeadingRadians())*Double.POSITIVE_INFINITY);
+                // Melee
+                // TODO: Investigate
+                _robot.setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyInfo.bearing - _robot.getRadarHeadingRadians())
+                                                * Double.POSITIVE_INFINITY);
         }
         else
-			// Victory dance?
+            // Victory dance?
             _robot.setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
     }
-	
-	@Override
-	public void onRobotDeath(RobotDeathEvent e){
+    
+    @Override
+    public void onRobotDeath(RobotDeathEvent e){
         Memory.enemies.remove(e.getName());
     }
 }
